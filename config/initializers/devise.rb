6f1f8d8feb083ee6f1fa117465ba42b2fc02ab41
Hakeem <babahakeem@outlook.com>
2021-08-05 20:05:55 +0200

    testing api calls

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -22,7 +22,7 @@ Devise.setup do |config|
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'user-read-email user-top-read playlist-modify-public user-library-read user-library-modify'
+  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'user-read-email user-top-read user-read-recently-played playlist-modify-public user-library-read user-library-modify'
 end
 
   # ==> Controller configuration
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/auth/spotify/callback', to: 'users#spotify'
-
   get '/auth/spotify', to: 'users#spotify'
+
+  get 'top_tracks', to: "users#top_tracks"
+  get 'top_artists', to: "users#top_artists"
+  get 'recently_played', to: "users#recently_played"
+
 end
