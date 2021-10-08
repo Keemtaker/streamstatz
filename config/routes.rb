120c713a29c44678ab2a25fe1e26ff2a43b3dea6
Hakeem <babahakeem@outlook.com>
2021-10-08 15:26:15 +0200

    dynamic error pages?

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,9 @@ Rails.application.routes.draw do
     get 'create_artists_pdf', to: "spotify_users#create_artists_pdf"
 
     post 'spotify_create_playlist', to: "spotify_users#spotify_create_playlist"
+
+    match "/404", to: "errors#not_found", via: :all
+    match "/422", to: "errors#unacceptable", via: :all
+    match "/500", to: "errors#internal_server_error", via: :all
   end
 end
